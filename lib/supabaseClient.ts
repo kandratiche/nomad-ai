6855a6dfe58bcbbb656fb17c4ba86d2e2f9ba0f5
kandratiche <cgnipnoob@gmail.com>
2026-03-02 11:16:04 +0500

    env test

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -4,6 +4,9 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+const EXPO_PUBLIC_SUPABASE_URL="https://nznswtsayytpdbijjqhj.supabase.co"
+
+const EXPO_PUBLIC_SUPABASE_ANON_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56bnN3dHNheXl0cGRiaWpqcWhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyNzkyODksImV4cCI6MjA4NTg1NTI4OX0.kSc7hLNkaH6n6wGP0Hrxj76467airWjh-m9qCPNZ1GU"
 
 if (!supabaseUrl || !supabaseAnonKey) {
   console.warn(
@@ -13,8 +16,8 @@ if (!supabaseUrl || !supabaseAnonKey) {
 }
 
 const supabase = createClient(
-  supabaseUrl || 'https://placeholder.supabase.co',
-  supabaseAnonKey || 'placeholder-anon-key',
+    EXPO_PUBLIC_SUPABASE_URL,
+    EXPO_PUBLIC_SUPABASE_ANON_KEY,
   {
     auth: {
       storage: AsyncStorage,
